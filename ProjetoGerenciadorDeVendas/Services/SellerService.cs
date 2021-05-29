cbabfc209e3452714bc2a4521750c68967b41b0e
Guilherme Neves <guilherme.nevescmp@gmail.com>
2021-05-29 01:24:26 -0300

    Criação do Serviço Seller
    Divisão do Serviço no projeto e Criação do Serviço Seller.

---
+++ ProjetoGerenciadorDeVendas/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using ProjetoGerenciadorDeVendas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoGerenciadorDeVendas.Services
+{
+    public class SellerService
+    {
+        private readonly ProjetoGerenciadorDeVendasContext _context;
+
+        public SellerService(ProjetoGerenciadorDeVendasContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- ProjetoGerenciadorDeVendas/Startup.cs
+++ ProjetoGerenciadorDeVendas/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ProjetoGerenciadorDeVendas.Models;
 using ProjetoGerenciadorDeVendas.Data;
+using ProjetoGerenciadorDeVendas.Services;
 
 namespace ProjetoGerenciadorDeVendas
 {
@@ -42,6 +43,7 @@ namespace ProjetoGerenciadorDeVendas
                     builder.MigrationsAssembly("ProjetoGerenciadorDeVendas")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
