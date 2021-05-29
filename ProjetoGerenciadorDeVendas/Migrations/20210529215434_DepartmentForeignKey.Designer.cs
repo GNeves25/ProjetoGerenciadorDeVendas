bb66403cdc8d9fb43b663bf375d9ad0df4ad7286
Guilherme Neves <guilherme.nevescmp@gmail.com>
2021-05-29 19:05:27 -0300

    Inclusão Foreign Key
    Inclusão do Foreign Key e de uma nova migration.

--- ProjetoGerenciadorDeVendas/Migrations/20210529215434_DepartmentForeignKey.Designer.cs
+++ ProjetoGerenciadorDeVendas/Migrations/20210529215434_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoGerenciadorDeVendas.Models;
 
 namespace ProjetoGerenciadorDeVendas.Migrations
 {
     [DbContext(typeof(ProjetoGerenciadorDeVendasContext))]
-    partial class ProjetoGerenciadorDeVendasContextModelSnapshot : ModelSnapshot
+    [Migration("20210529215434_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
