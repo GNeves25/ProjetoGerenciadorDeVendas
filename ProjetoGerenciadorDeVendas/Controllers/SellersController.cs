6e2ce4b9380cde89842db264c2560e7ab9f35cd4
Guilherme Neves <guilherme.nevescmp@gmail.com>
2021-05-29 20:01:40 -0300

    Criação do Delete
    Criação da deleção dos vendedores.

--- ProjetoGerenciadorDeVendas/Controllers/SellersController.cs
+++ ProjetoGerenciadorDeVendas/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjetoGerenciadorDeVendas.Models;
+using ProjetoGerenciadorDeVendas.Models.ViewModels;
 using ProjetoGerenciadorDeVendas.Services;
 using System;
 using System.Collections.Generic;
@@ -11,10 +12,12 @@ namespace ProjetoGerenciadorDeVendas.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -25,7 +28,9 @@ namespace ProjetoGerenciadorDeVendas.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -33,6 +38,31 @@ namespace ProjetoGerenciadorDeVendas.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
---
+++ ProjetoGerenciadorDeVendas/Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using ProjetoGerenciadorDeVendas.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ProjetoGerenciadorDeVendas.Services
+{
+    public class DepartmentService
+    {
+        private readonly ProjetoGerenciadorDeVendasContext _context;
+
+        public DepartmentService(ProjetoGerenciadorDeVendasContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll() 
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
