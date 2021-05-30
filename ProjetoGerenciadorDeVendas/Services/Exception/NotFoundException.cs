c1b7b325ceb7e7bb4cdf34c20bba202ebb8efa0c
Guilherme Neves <guilherme.nevescmp@gmail.com>
2021-05-30 11:10:34 -0300

    Criação das Exceções
    Criação das exceções personalizadas, correção do edit de vendedores.

---
+++ ProjetoGerenciadorDeVendas/Services/Exception/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoGerenciadorDeVendas.Services.Exception
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
