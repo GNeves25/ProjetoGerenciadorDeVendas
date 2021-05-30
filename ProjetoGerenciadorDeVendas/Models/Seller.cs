d2c2c64b90ea1d05941e807081eaaafbb85ab31d
Guilherme Neves <guilherme.nevescmp@gmail.com>
2021-05-30 14:58:58 -0300

    Correção de Validação
    Correção das validações quando o JS está desabilitado.

--- ProjetoGerenciadorDeVendas/Models/Seller.cs
+++ ProjetoGerenciadorDeVendas/Models/Seller.cs
@@ -8,22 +8,35 @@ namespace ProjetoGerenciadorDeVendas.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0}: Campo Requerido")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} inválido, {2} até {1}")]
         [Display(Name = "Nome")]
         public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [Display(Name = "E-mail")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0}: Campo Requerido")]
         [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0}: Campo Requerido")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Salário Base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         [Display(Name = "Departamento")]
         public Department Department { get; set; }
+
         [Display(Name = "Departamento")]
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
