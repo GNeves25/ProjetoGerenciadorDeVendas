45b379ed8032b14d0c9643cc7f44ed2a57091ec5
Guilherme Neves <guilherme.nevescmp@gmail.com>
2021-05-31 21:13:39 -0300

    Alteração das Pesquisas
    Alteração das Pesquisas em grupo e única.

---
+++ ProjetoGerenciadorDeVendas/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using ProjetoGerenciadorDeVendas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoGerenciadorDeVendas.Services
+{
+    public class SalesRecordService
+    {
+        private readonly ProjetoGerenciadorDeVendasContext _context;
+
+        public SalesRecordService(ProjetoGerenciadorDeVendasContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
