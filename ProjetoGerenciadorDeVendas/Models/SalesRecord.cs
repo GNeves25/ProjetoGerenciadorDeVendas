e000edf7d31bc392337254adec7830834ecc746b
Guilherme Neves <guilherme.nevescmp@gmail.com>
2021-05-31 22:01:42 -0300

    Implementação da Pesquisa em Grupo
    Implementação da Pesquisa em Grupo.

--- ProjetoGerenciadorDeVendas/Models/SalesRecord.cs
+++ ProjetoGerenciadorDeVendas/Models/SalesRecord.cs
@@ -1,14 +1,21 @@
 ﻿using ProjetoGerenciadorDeVendas.Models.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoGerenciadorDeVendas.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
+
         public SaleStatus Status { get; set; }
+
         public Seller Seller { get; set; }
 
         public SalesRecord()
